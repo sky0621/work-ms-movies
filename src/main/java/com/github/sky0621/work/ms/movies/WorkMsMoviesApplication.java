1d8efa274323defe73228f0c3756f6881aa44799
S.S <ss@ss.com>
2016-10-01 06:33:55 +0900

    first

---
+++ src/main/java/com/github/sky0621/work/ms/movies/WorkMsMoviesApplication.java
@@ -0,0 +1,12 @@
+package com.github.sky0621.work.ms.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkMsMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkMsMoviesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/sky0621/work/ms/movies/WorkMsMoviesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.sky0621.work.ms.movies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkMsMoviesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
