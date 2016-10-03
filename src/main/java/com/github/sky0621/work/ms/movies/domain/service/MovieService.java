a84e7fd1fe6b8da027fb019b27b2093e11bd0e92
S.S <ss@ss.com>
2016-10-03 09:54:14 +0900

    db get

--- src/main/java/com/github/sky0621/work/ms/movies/domain/service/MovieService.java
+++ src/main/java/com/github/sky0621/work/ms/movies/domain/service/MovieService.java
@@ -1,13 +1,11 @@
 package com.github.sky0621.work.ms.movies.domain.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.github.sky0621.work.ms.movies.domain.entity.Movie;
 import com.github.sky0621.work.ms.movies.domain.repository.MovieRepository;
 import com.github.sky0621.work.ms.movies.domain.resource.MovieResource;
 
@@ -17,12 +15,9 @@ public class MovieService {
 	@Autowired
 	MovieRepository movieRepository;
 
-	// FIXME ダミー実装のためのメモリ保持リスト
-	static List<MovieResource> movieResources = new ArrayList<>();
-
 	public List<MovieResource> findAll() {
-		List<Movie> movieList = movieRepository.findAll();
-		movieResources = movieList.stream().map(movie -> {
+		// エンティティからリソースへの詰め替え
+		return movieRepository.findAll().stream().map(movie -> {
 			MovieResource resource = new MovieResource();
 			resource.setId(movie.getId());
 			resource.setName(movie.getName());
@@ -30,15 +25,6 @@ public class MovieService {
 			resource.setPath(movie.getPath());
 			return resource;
 		}).collect(Collectors.toList());
-
-		// FIXME ダミー実装
-		// MovieResource res = new MovieResource();
-		// res.setId("000000-000000-00000000-00000000");
-		// res.setName("サンプル動画");
-		// res.setDatetime(LocalDate.of(2016, 10, 1));
-		// res.setPath("sample.mp4");
-		// movieResources.add(res);
-		return movieResources;
 	}
 
 }
